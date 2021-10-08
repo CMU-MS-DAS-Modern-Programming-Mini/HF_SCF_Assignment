8149c0cbfc05e5c5ff1964d3a2fcee46dffbca00
Manish Sainani <msainani@andrew.cmu.edu>
2021-10-08 13:36:45 -0400

    tests

--- tests/SCF/test_SCF.py
+++ tests/SCF/test_SCF.py
@@ -12,7 +12,7 @@ Vuv = pickle.load(open("vuv.pkl", "rb"))
 # Electron Repulsion 2 electron integrals
 eri = pickle.load(open("eri.pkl", "rb"))
 
-def test_calc_nuclear_repulsion_energy(mol_h2o): #checks nuclear repulsion energy and gives out error messaage if incorrect
+def test_calc_nuclear_repulsion_energy(mol_h2o):  # checks nuclear repulsion energy and gives out error messaage if incorrect
     assert SCF.calc_nuclear_repulsion_energy(mol_h2o) == 8.00236706181077,"Nuclear Repulsion Energy Test (H2O) Failed"
     
 def test_calc_initial_density(mol_h2o): 
@@ -25,7 +25,7 @@ def test_calc_initial_density(mol_h2o):
     assert Duv.sum() == 0.0
     assert Duv.shape == (mol_h2o.nao,mol_h2o.nao)
 
-def test_calc_hcore_matrix(): #checks Hcore matrix
+def test_calc_hcore_matrix():  # checks Hcore matrix
     Tuv = pickle.load(open("tuv.pkl", "rb"))
     Vuv = pickle.load(open("vuv.pkl", "rb"))
     Huv = SCF.calc_hcore_matrix(Tuv, Vuv)
@@ -33,22 +33,22 @@ def test_calc_hcore_matrix(): #checks Hcore matrix
     assert Huv[3, 4] == 0.0
 
 
-def test_calc_fock_matrix(mol_h2o): #checks fock matrix
+def test_calc_fock_matrix(mol_h2o):  # checks fock matrix
     Huv = SCF.calc_hcore_matrix(Tuv, Vuv)
     Duv = SCF.calc_initial_density(mol_h2o)
     Fuv= SCF.calc_fock_matrix(mol_h2o, Huv, eri, Duv)
     assert Fuv[0,0] == -32.57739541261037
-    assert Fuv[2,5] == pytest.approx(-1.6751501447185015) #needed because the answers are not exactly equal
+    assert Fuv[2,5] == pytest.approx(-1.6751501447185015)  # needed because the answers are not exactly equal
     assert Fuv[5,2] == pytest.approx(-1.6751501447185015)
 
-def test_solve_Roothan_equations(mol_h2o): #checks Roothan euations (eigen vectors and eigen values)
+def test_solve_Roothan_equations(mol_h2o):  # checks Roothan euations (eigen vectors and eigen values)
     Huv = SCF.calc_hcore_matrix(Tuv, Vuv)
     Duv = SCF.calc_initial_density(mol_h2o)
     Fuv= SCF.calc_fock_matrix(mol_h2o, Huv, eri, Duv)
     mo_e, mo_c = SCF.solve_Roothan_equations(Fuv, Suv)
     assert mo_e[0] == pytest.approx(-32.5783029) # checking one value from the array
 
-def test_calc_total_energy(mol_h2o): #checks total energy
+def test_calc_total_energy(mol_h2o):  # checks total energy
     Huv = SCF.calc_hcore_matrix(Tuv, Vuv)
     Duv = SCF.calc_initial_density(mol_h2o)
     Fuv= SCF.calc_fock_matrix(mol_h2o, Huv, eri, Duv)
@@ -56,7 +56,7 @@ def test_calc_total_energy(mol_h2o): #checks total energy
     Etot_new = SCF.calc_total_energy(Fuv, Huv, Duv, Enuc)
     assert Etot_new ==  pytest.approx(8.0023670618)
 
-def test_form_density_matrix(mol_h2o): #checks Duv new over 1 iteration
+def test_form_density_matrix(mol_h2o):  # checks Duv new over 1 iteration
     
     Huv = SCF.calc_hcore_matrix(Tuv, Vuv)
     Duv = SCF.calc_initial_density(mol_h2o)
